98bfe1b97c351aeca4bb0d96daf2672b277a31fc
adrianchesneau <adrian.chesneau@gmail.com>
2024-11-25 15:18:34 +0100

    add nodemon

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,22 @@ const port = 5000;
 const mysql = require('mysql');
 const db = mysql.createConnection({   host: "localhost", port:'8889',   user: "root",   password: "root", database:'product_checker'});
 
-
-
-
-
+db.connect(function(err) { 
+    if (err) throw err;   
+    console.log("Connecté à la base de données MySQL!"); 
+  
+  });
+  
 app.get('/api', (req, res) => {
-
-    db.connect(function(err) { 
-          if (err) throw err;   
-          console.log(); 
-        
-        });
-
-
-
-
-
+    db.query("SELECT * from products" , function(err, result) {
+        console.log(err, result)
+        if(err){
+            res.send(err)
+        }else{
+            res.send(result[0].name)
+        }
+    })
+   
 
 });
 
