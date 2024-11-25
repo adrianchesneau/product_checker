622f64e24eda398c8babbccfb956af5c22b82591
adrianchesneau <adrian.chesneau@gmail.com>
2024-11-25 14:04:14 +0100

    envoi front

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,30 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mysql = require('mysql');
+const db = mysql.createConnection({   host: "localhost",   user: "root",   password: "" });
+
+
+
+
+
 app.get('/api', (req, res) => {
-    res.send('Hello depuis le backend !');
+
+
+    db.connect(function(err) {   
+        if (err) {
+            res.send(err);
+            return
+        }
+        console.log("Connecté à la base de données MySQL!"); 
+        res.send('Hello depuis le backend !');
+      });
+
+
+
+
+
+
 });
 
 app.listen(port, () => {
